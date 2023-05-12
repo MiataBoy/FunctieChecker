cd6bb55b6b57f6fd6abd9b339ac003ed9551a5c2
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-12 10:52:07 +0200

    add function queries

--- js/gpt.js
+++ js/gpt.js
@@ -1,12 +1,12 @@
-const functionNames = "The name of the function must describe exactly what the function does, and must start with a verb. Additionally, the entire function must be comprised in ONE language and be snake_case."
-const parameterNames = "Parameter names must describe exactly what the parameter is for, must be comprised of ONE language and must be snake_case."
-const returnValues = "The return value should remain useful to the user under any circumstance."
-const localVars = "Used variables must only be local, and must be comprised of ONE language, global variables must only be used as a last resort and only if there is no other way around it."
-const oneJob = "The function must be dedicated to a single task, and must not serve multiple."
-const reusable = "The function must be reusable under any circumstance where it could be usable."
-const printInstruction = "The function must not contain any print instructions, unless the function is dedicated to printing something."
-const inputInstruction = "The function must not contain any input instructions, unless the function is dedicated to receiving, and/or submitting inputs."
-const unrecoverableErrors = "The function must not contain error raises-, exits or prints"
+const functionNames = " The name of the function must describe exactly what the function does, and must start with a verb. Additionally, the entire function must be comprised in ONE language and be snake_case."
+const parameterNames = " Parameter names must describe exactly what the parameter is for, must be comprised of ONE language and must be snake_case."
+const returnValues = " The return value should remain useful to the user under any circumstance."
+const localVars = " Used variables must only be local, and must be comprised of ONE language, global variables must only be used as a last resort and only if there is no other way around it."
+const oneJob = " The function must be dedicated to a single task, and must not serve multiple."
+const reusable = " The function must be reusable under any circumstance where it could be usable."
+const printInstruction = " The function must not contain any print instructions, unless the function is dedicated to printing something."
+const inputInstruction = " The function must not contain any input instructions, unless the function is dedicated to receiving, and/or submitting inputs."
+const unrecoverableErrors = " The function must not contain error raises-, exits or prints"
 
 async function fetchCompletions(text, defaultContent) {
     const url = "https://free.churchless.tech/v1/chat/completions";
